49239fcee929d970a9cf34c1010c90cc6731e4f1
pabloperguz <p.perez-guzman@imperial.ac.uk>
2025-04-29 08:57:41 +0100

    added infection_data script and tests

---
+++ tests/testthat/test-country_data.R
@@ -0,0 +1,8 @@
+# snapshot tests for country data to flag unexpected changes
+# check some countries
+test_countries <- c("China", "United Kingdom", "Canada")
+test_that("daedalus::country_data: snapshot tests", {
+  expect_snapshot(daedalus::country_data[test_countries])
+
+  expect_snapshot(daedalus::country_data[["Canada"]])
+})
